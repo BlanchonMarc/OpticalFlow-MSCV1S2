ef2c87aa51f9d167fad83585e5e7bd7c36241c20
BlanchonMarc <fr.marc.blanchon@gmail.com>
2017-05-24 10:59:20 +0200

    NewUpdate

--- TP/code/Lab.m
+++ TP/code/Lab.m
@@ -1,9 +1,13 @@
-im1 = imread('/Users/marc/Documents/MsCV/Semester 2/Autonomous Robotics/OpticalFlowClass/TP/sequences/garden/garden_1.png');
-im2 = imread('/Users/marc/Documents/MsCV/Semester 2/Autonomous Robotics/OpticalFlowClass/TP/sequences/garden/garden_2.png');
+im1 = imread('../sequences/garden/garden_1.png');
+im2 = imread('../sequences/garden/garden_2.png');
 
 ite = 100;
 lambda = 0.1;
 
-[u,v] = HS(im1, im2 , lambda , ite);
+%[u,v] = HS(im1, im2 , lambda , ite);
 
-plotOF_arrows(u,v)
+%plotOF_arrows(u,v)
+
+[u1,v1] = LK(im1, im2 , 2);
+
+plotOF_arrows(u1,v1)
